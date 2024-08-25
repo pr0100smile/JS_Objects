b997ec0840c3c6cc4ae49d9180e89307ca016695
Dmitriy NetoIT <pr0100_smile@mail.ru>
2024-08-25 12:00:18 +0300

    Adding an active class

--- Tabs/task.js
+++ Tabs/task.js
@@ -1,26 +1,25 @@
-const tabItems = Array.from(document.querySelectorAll('.tab'))
-const contentItems = Array.from(document.querySelectorAll('.tab__content'))
+const tabItems = Array.from(document.querySelectorAll('.tab'));
+const contentItems = Array.from(document.querySelectorAll('.tab__content'));
 
 const clearActiveClass = (element, className = 'tab__content_active') => {
-    element.find(item => item.classList.remove(`${ className }`))
+    element.forEach(item => item.classList.remove(className));
 }
 
 const setActiveClass = (element, index, className = 'tab__content_active') => {
-    element[index].classList.add(`${ className }`)
+    element[index].classList.add(className);
 }
 
 const checkoutTabs = (item, index) => {
     item.addEventListener('click', () => {
-        
-        if (item.classList.contains('tab__content_active')) return
-        console.log(item)
+        if (item.classList.contains('tab__content_active')) return;
+        console.log(item);
 
-        clearActiveClass(tabItems)
-        clearActiveClass(contentItems)
+        clearActiveClass(tabItems);
+        clearActiveClass(contentItems);
 
-        setActiveClass(tabItems, index)
-        setActiveClass(contentItems, index)
-    })
+        setActiveClass(tabItems, index);
+        setActiveClass(contentItems, index);
+    });
 }
 
-tabItems.forEach(checkoutTabs)
+tabItems.forEach(checkoutTabs);
